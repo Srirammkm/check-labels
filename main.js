10d4f2612bc2320745ce7f60ad4ab697f711e286
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2022-11-22 16:17:14 +0530

    removed gha

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 
 async function run() { 
-    const token = "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" //core.getInput("github-token", { required: true }); //ewfw
+    const token = core.getInput("github-token", { required: true });
     const octokit = github.getOctokit(token);
 
     const labelNames = await getPullRequestLabelNames(octokit);
@@ -13,12 +13,13 @@ async function run() {
         (label) => labelNames.findIndex((value) => label === value) >= 0
     );
     const services = [];
+    var monthly_release = "";
     labelNames.forEach((value) => { 
         if(value.startsWith("update-") && !value.endsWith("all")){
             services.push(value.replace("update-", ""));
         }
-        if(value == "monthly-release"){
-
+        if(value.startsWith("MR-")){
+            monthly_release = value.replace("MR-","");
         }
     });
     const ftBranch = [];
@@ -30,10 +31,10 @@ async function run() {
     const octo = new Octokit({
         auth: token,
         });
-    async function test(file){
+    async function get_content(file){
         content = await octo.rest.repos.getContent({
-                    owner: "sriram-demo",
-                    repo: "demo",
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
                     path: `taskdefinitions/${file}.json`
                 }).then(function(response){
                     let data = response["data"]["content"];
@@ -44,12 +45,12 @@ async function run() {
                 });
         return content
     }
-    test("R2022.11.S").then(function(response){
+    get_content(monthly_release).then(function(response){
         var dict = {};
         const stages = response["stages"]
         var count = 0
         for (const index in stages) { 
-            test(stages[index]).then(function(response){
+            get_content(stages[index]).then(function(response){
                 dict[stages[index]] = response
                 count++
                 if(count == stages.length){
@@ -67,7 +68,7 @@ async function run() {
 async function getPullRequestLabelNames(octokit) {
     const owner = github.context.repo.owner;
     const repo = github.context.repo.repo;
-    const commit_sha = "5a0724773b529a4c87c0e508c8ef6b516843cb55"//github.context.sha;
+    const commit_sha =  github.context.sha;
 
     const response =
         await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
@@ -81,7 +82,7 @@ async function getPullRequestLabelNames(octokit) {
 }
 
 function getInputLabels() {
-    const raw = `["adhoc-task"]`//core.getInput("labels", { required: true });
+    const raw = core.getInput("labels", { required: true });
     const json = JSON.parse(raw);
     return Array.isArray(json) ? json : [];
 }
